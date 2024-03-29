00fce33472329b471b3768fa56b04bba032f7b6c
eunjeong-joy <the.ej.joy@gmail.com>
2024-03-29 18:53:48 +0900

    TASK-2 : MainActivity 위치 이동
    - presentation으로 이동
    - app module 의존성 추가
    - 불필요한 코드 제거

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,7 @@ android {
 
 dependencies {
     implementation(project(":data"))
+    implementation(project(":presentation"))
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
