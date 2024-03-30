9f38826c5e912d46e463dbf3eb202ed66c37ba35
eunjeong-joy <the.ej.joy@gmail.com>
2024-03-30 15:57:59 +0900

    TASK-4 : 1,2번 섹션에 추가될 product view 추가

---
+++ presentation/src/main/java/com/example/presentation/product/view/SmallProduct.kt
@@ -0,0 +1,14 @@
+package com.example.presentation.product.view
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+
+@Preview
+@Composable
+fun PreviewSmallProduct() {
+    SmallProduct()
+}
+@Composable
+fun SmallProduct() {
+}
