9972ae2bcc01474f44ad3cc754369081bdee2621
eunjeong-joy <the.ej.joy@gmail.com>
2024-03-30 13:32:03 +0900

    TASK-3 : 불필요한 코드 제거

--- domain/src/main/java/com/example/domain/section/model/SectionEntity.kt
+++ domain/src/main/java/com/example/domain/section/model/SectionEntity.kt
@@ -5,15 +5,4 @@ data class SectionEntity(
     val id: Int,
     val type: String,
     val url: String
-) {
-    companion object {
-        private fun dummy() = SectionEntity(
-            title = "TEST",
-            id = 1,
-            type = "A",
-            url = "url"
-        )
-
-        fun dummyList() = listOf(dummy(), dummy(), dummy())
-    }
-}
+)
