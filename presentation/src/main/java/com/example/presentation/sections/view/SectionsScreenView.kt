710c40b8e7259c22cf23bd85d4efcf8c45916826
eunjeong-joy <the.ej.joy@gmail.com>
2024-04-02 20:56:14 +0900

    TASK-8 : BigProduct 목록 SectionsView에 추가

--- presentation/src/main/java/com/example/presentation/sections/view/SectionsScreenView.kt
+++ presentation/src/main/java/com/example/presentation/sections/view/SectionsScreenView.kt
@@ -2,7 +2,9 @@ package com.example.presentation.sections.view
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Text
 import androidx.compose.material.pullrefresh.PullRefreshIndicator
@@ -16,8 +18,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.presentation.product.view.BigProduct
 import com.example.presentation.product.view.GridSection
 import com.example.presentation.product.view.HorizontalSection
+import com.example.presentation.product.view.SectionTitle
 import com.example.presentation.sections.data.SectionType
 import com.example.presentation.sections.viewmodel.SectionsViewModel
 
@@ -69,28 +73,57 @@ fun SectionsScreenView(
                         }
 
                         SectionType.VERTICAL -> {
-                            //TODO : VERTICAL section 작업 예정
+                            if (section.products.isEmpty()) {
+                                viewModel.fetchProducts(section.id)
+                                item {
+                                    Text(text = "Loading") //임시 loading UI
+                                }
+                            } else {
+                                item {
+                                    SectionTitle(
+                                        modifier = Modifier.padding(
+                                            start = 15.dp,
+                                            end = 15.dp,
+                                            top = 15.dp
+                                        ),
+                                        title = section.title
+                                    )
+                                }
+
+                                items(section.products) { product ->
+                                    BigProduct(
+                                        modifier = Modifier.padding(
+                                            start = 15.dp,
+                                            end = 15.dp,
+                                            top = 15.dp
+                                        ),
+                                        product = product
+                                    )
+                                }
+
+                            }
                         }
 
                         SectionType.GRID -> {
                             item {
-                                    GridSection(
-                                        section = section
-                                    )
-                                }
+                                GridSection(
+                                    section = section
+                                )
                             }
-
-                            else -> {} // nothing
                         }
+
+                        else -> {} // nothing
                     }
                 }
-            } else {
-                Text(text = "section loading") //임시 loading UI
             }
-            PullRefreshIndicator(
-                modifier = Modifier.align(Alignment.TopCenter),
-                refreshing = refreshState ?: false,
-                state = pullRefreshState
-            )
+        } else {
+            Text(text = "section loading") //임시 loading UI
         }
+
+        PullRefreshIndicator(
+            modifier = Modifier.align(Alignment.TopCenter),
+            refreshing = refreshState ?: false,
+            state = pullRefreshState
+        )
     }
+}
